6e895c592c7c966c409dc3ee618886591a3e6660
chadfennell <libsys@gmail.com>
2016-11-19 14:06:10 -0700

    use the api

--- lib/cdmbl/default_cdm_notification.rb
+++ lib/cdmbl/default_cdm_notification.rb
@@ -2,7 +2,7 @@ module CDMBL
   # An example callback
   class DefaultCdmNotification
     def self.call!(collection, id, endpoint)
-      puts "A CONTENTdm API request notification"
+      puts "A CONTENTdm API request notification: #{endpoint} #{collection}:#{id}"
     end
   end
 end
