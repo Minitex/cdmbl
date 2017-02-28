b3ca8237634f717605a5fd9fbdcf9df3b45cc4b6
chadfennell <libsys@gmail.com>
2017-02-28 10:20:16 -0600

    from is now optional

--- test/oai_request_test.rb
+++ test/oai_request_test.rb
@@ -16,7 +16,7 @@ module CDMBL
 
     describe 'when no resumption token is present' do
       it 'requests the first batch' do
-      client.expect :get_response, client_response, [URI('http://example.com?verb=ListIdentifiers&metadataPrefix=oai_dc&from=1900-01-01')]
+      client.expect :get_response, client_response, [URI('http://example.com?verb=ListIdentifiers&metadataPrefix=oai_dc')]
       client_response.expect :body, 'foo'
         request = OaiRequest.new(base_uri: 'http://example.com', client: client)
         request.identifiers.must_equal 'foo'
