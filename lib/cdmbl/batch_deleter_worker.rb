c54a17eca21928560b5dd2a74419517bd5cc9fb9
Dan Olson <olson_dan@yahoo.com>
2021-09-04 21:41:17 -0400

    Allow pruning stale Solr records filtered by query
    This will allow for pruning records by collection, or any other set of
    criteria. We'll be able to pass a setspec to a rake task, or even add
    this to the MDL UI at some point.
    
    Addresses #145

--- lib/cdmbl/batch_deleter_worker.rb
+++ lib/cdmbl/batch_deleter_worker.rb
@@ -3,15 +3,16 @@ require 'sidekiq'
 module CDMBL
   class BatchDeleterWorker
     include Sidekiq::Worker
-    attr_reader :start, :batch_size, :prefix, :oai_url, :solr_url
+    attr_reader :start, :batch_size, :prefix, :oai_url, :solr_url, :solr_query
     attr_writer :batch_deleter_klass, :job_complete_notification, :solr_client
 
-    def perform(start = 0, batch_size = 10, prefix = '', oai_url = '', solr_url = '')
+    def perform(start = 0, batch_size = 10, prefix = '', oai_url = '', solr_url = '', solr_query = nil)
       @start      = start
       @batch_size = batch_size
       @prefix     = prefix
       @oai_url    = oai_url
       @solr_url   = solr_url
+      @solr_query = solr_query
       delete!
       batch_deleter
     end
@@ -36,19 +37,25 @@ module CDMBL
           batch_size,
           prefix,
           oai_url,
-          solr_url
+          solr_url,
+          solr_query
         )
       end
     end
 
     def batch_deleter
-      @batch_deleter ||= batch_deleter_klass.new(
-        start: start,
-        batch_size: batch_size,
-        prefix: prefix,
-        solr_client: solr_client,
-        oai_url: oai_url
-      )
+      @batch_deleter ||= begin
+        args = {
+          start: start,
+          batch_size: batch_size,
+          prefix: prefix,
+          solr_client: solr_client,
+          solr_query: solr_query,
+          oai_url: oai_url
+        }.compact
+
+        batch_deleter_klass.new(args)
+      end
     end
 
     def solr_client
--- test/default_solr_test.rb
+++ test/default_solr_test.rb
@@ -60,6 +60,35 @@ module CDMBL
         client.verify
         connection.verify
       end
+
+      it 'scopes the query when one is provided' do
+        mock_results = []
+        client.expect(
+          :connect,
+          connection,
+          [{url: "http://solr:8983/solr/some-core-here"}]
+        )
+        connection.expect(:get, mock_results, [
+          'select',
+          {
+            params: {
+              q: '*:*',
+              fq: 'setspec_ssi:otter',
+              defType: 'edismax',
+              fl: '',
+              rows: 10,
+              start: 0
+            }
+          }
+        ])
+        results = Solr.new(
+          url: 'http://solr:8983/solr/some-core-here',
+          client: client
+        ).ids(fq: 'setspec_ssi:otter')
+        _(results).must_equal(mock_results)
+        client.verify
+        connection.verify
+      end
     end
   end
 end
